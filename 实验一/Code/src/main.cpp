a3de03b75ed60e42b4dd1a6b14b1887cbdd096d7
AnthonyHaozeZhu <1119886232@qq.com>
2022-03-12 15:50:36 +0800

    开始

---
+++ 实验一/Code/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "test.h"
+
+int main()
+{
+    std::cout << "hello world" << std::endl;
+    std::cout << N << std::endl;
+}
---
+++ 实验一/Code/src/test1.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main()
+{
+    std::cout << "test1" << std::endl;
+}
